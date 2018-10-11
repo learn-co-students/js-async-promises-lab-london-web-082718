f12218a2b788fcc1f799da0e719ca72b7574c9a7
Ciaran M <cmorinan@gmail.com>
2018-10-11 14:05:56 +0100

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ const questions = [
 ]
 
 let question;
+const qContainer = document.getElementsByClassName('question-container')[0]
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(resolve =>
+    setTimeout( () => resolve(question), time)
+  )
+}
+
+function removeQuestion() {
+  qContainer.innerHTML = ''
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion(question)
+  return new Promise(resolve =>
+    setTimeout(() => removeQuestion, time)
+  )
+}
+
+function trueAndFalseButtons() {
+  green = document.getElementsByClassName('green')[0]
+  red = document.getElementsByClassName('red')[0]
+  return [green, red]
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(button => button.classList.toggle('hide'))
+}
+
+function appendQuestion(question) {
+  qContainer.innerHTML = question.questionText
+}
+
+function displayQuestionOnClick() {
+  askQuestionThenRemoveQuestion(3000)
+  toggleTrueAndFalseButtons()
+}
+
+goBtn = document.getElementsByClassName('waves-effect')[0]
+goBtn.addEventListener('click', displayQuestionOnClick)
+
